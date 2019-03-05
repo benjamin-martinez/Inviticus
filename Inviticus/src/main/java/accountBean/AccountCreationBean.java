f565c7b620431e1b688824875bf61c753f4c4e4e
Benjamin Martinez <benjamin.martiinez@gmail.com>
2019-03-04 22:30:23 -0500

    More modifiations to accountBean package.

--- Inviticus/src/main/java/accountBean/AccountCreationBean.java
+++ Inviticus/src/main/java/accountBean/AccountCreationBean.java
@@ -5,6 +5,8 @@
  */
 package accountBean;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This class tests the logic for the account.
@@ -19,6 +21,7 @@ package accountBean;
 public class AccountCreationBean{
     
     private Account newAccount;
+    private boolean isUpperCase = Character.isUpperCase(0);
     
     public AccountCreationBean(Account newAccount) {
         this.newAccount = newAccount;
@@ -36,23 +39,47 @@ public class AccountCreationBean{
     }
     
     /**
-     * Username needs to be at least 5 letters
+     * Username needs to be at least 6 letters
      */
     public boolean verifyUsername() {
-        return false;
+        if(newAccount.getUsername().length() < 6)
+            return false;
+        return true;
     }
     
     /**
      * Checks to see if the email is a valid email
      */
     public boolean verifyEmail() {
-        return false;
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+                            "[a-zA-Z0-9_+&*-]+)*@" +
+                            "(?:[a-zA-z0-9-]+\\.)+[a-z" +
+                            "A-Z]{2,7}$";
+        
+        Pattern pat = Pattern.compile(emailRegex);
+        if(newAccount.getEmail() == null)
+            return false;
+        
+        // before this return I need to see if the email exists
+        return true;
     }
     
     /**
      * Minimum length should be 6 with at least 1 uppercase letter
      */
     public boolean verifyPassword() {
+        if(newAccount.getPassword().length() < 6)
+            return false;
+        
+        boolean upperCaseFlag = false;
+        for(int i = 0; i < newAccount.getPassword().length(); i++) {
+            Character ch = newAccount.getPassword().charAt(i);
+            if(Character.isUpperCase(ch))
+                upperCaseFlag = true;
+        }
+        
+        if(upperCaseFlag)
+            return true;
         return false;
     }
     
